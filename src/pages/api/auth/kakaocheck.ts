5815aa37762d26ab1f6b732b4c43692f0d6d117d
HayeongShin <judy.hayoung.shin@gmail.com>
2023-11-16 15:34:47 +0900

    [ADD] googlecheck, kakaocheck 이전으로 되돌리기

--- src/pages/api/auth/kakaocheck.ts
+++ src/pages/api/auth/kakaocheck.ts
@@ -12,8 +12,6 @@ async function getTokenFromKakao(authCode: string) {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'Server is running' });
-
   const { code } = req.query;
 
   if (code && typeof code === 'string') {
